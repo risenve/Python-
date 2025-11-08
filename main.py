28cc0d54b3cfb8e33950e280d75a247f1eebc51a
Ripsime <hripsimesargsyan90575@gmail.com>
2025-11-08 16:16:53 +0400

    update: added nohup script

--- main.py
+++
@@ -1,7 +0,0 @@
-config = {}
-with open("config/config.txt") as f:
-    for line in f:
-        key, value = line.strip().split("=")
-        config[key] = int(value)
-
-print("interval -- ", config["interval"])
